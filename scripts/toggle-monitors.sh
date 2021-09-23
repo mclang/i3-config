191e77e909160fb3ede8ac588886d5c9164c6358
mclang <jani.long@gmail.com>
2021-09-23 18:01:58 +0300

    Adds framerate detection and setup

--- scripts/toggle-monitors.sh
+++ scripts/toggle-monitors.sh
@@ -7,9 +7,8 @@
 # ```
 #
 # NOTES:
-# - Using `--auto` and `--right-of `simplifies things, no need to know resolution for `--pos` calculation.
-# - Leaving `sed` into multiline format b/c resolution or refresh rates might be needed later!
-#
+# - Using `--right-of `simplifies things b/c no need to know resolution for `--pos` calculation.
+# - Using `--auto` is not possible if `--rate` is used b/c the latter works only with `--mode`.
 set -e
 set -u
 
@@ -18,30 +17,49 @@ if ! hash xrandr 2>/dev/null; then
 	exit 1
 fi
 
-# https://www.gnu.org/software/sed/manual/html_node/Text-search-across-multiple-lines.html
-CONNECTED=$(xrandr -q | sed -En '{N; s/(\w+)\s+connected\s+(primary)?[^\n]+\n\s+([0-9]{4}x[0-9]{4}).+/\1:\3:\2/p ; D}')
-DISCONNECTED=$(xrandr -q | sed -En 's/(\w+)\s+disconnected.+/\1/p')
+# Returns the largest number from `xrandr` input like '59.95 + 143.91'.
+function parse_refresh_rate {
+	local maxrt=0
+	# echo "rates: '$1'" >&2
+	IFS=' +*' read -ra rates <<< "$1"
+	for rate in ${rates[@]}; do
+		local rt="${rate%.*}"       # '144.00' -> '144'
+		((rt > maxrt)) && maxrt=$rt
+	done
+	unset rates
+	echo "$maxrt"
+}
+
+# Array for `xrandr` output parameters
 XRANDR_PARAMETERS=()
 
-for LINE in ${CONNECTED[@]}; do
+# https://mywiki.wooledge.org/BashFAQ/001
+# https://www.gnu.org/software/sed/manual/html_node/Text-search-across-multiple-lines.html
+
+# Example results from `xrandr`
+# eDP:primary:1920x1080:144.00*+  60.00
+# HDMI-A-0::2560x1440:59.95 + 143.91
+while read -r LINE; do
 	IFS=':' read -ra PARTS <<< "$LINE"
-	PORT="${PARTS[0]}"
-	# RESO="${PARTS[1]}"
-	if (( ${#PARTS[@]} == 3 )); then
+	OUTPUT="${PARTS[0]}"
+	if [[ "${PARTS[1]}" == "primary" ]]; then
 		POSITION="--primary"
 	else
 		POSITION="--right-of $PREVIOUS"
 	fi
-	XRANDR_PARAMETERS+=("--output $PORT --auto $POSITION")
-	PREVIOUS="$PORT"
-done
+	RESOLUTION="${PARTS[2]}"
+	REFRESHRATE=$(parse_refresh_rate "${PARTS[3]}")
+	XRANDR_PARAMETERS+=("--output $OUTPUT --mode $RESOLUTION --rate $REFRESHRATE $POSITION")
+	PREVIOUS="$OUTPUT"
+done < <(xrandr -q | sed -En '{N; s/(\w+)\s+connected\s+(primary)?[^\n]+\n\s+([0-9]{4}x[0-9]{4})\s+([0-9 .+*]+)/\1:\2:\3:\4/p ; D}')
 
-# Do NOT use `"${DISCONNECTED[@]}"` b/c it preservers also linebreaks between ports
-for LINE in ${DISCONNECTED[@]}; do
-	XRANDR_PARAMETERS+=("--output $LINE --auto")
-done
+
+# Using `--auto` sets disconnected outputs off
+while read -r OUTPUT; do
+	XRANDR_PARAMETERS+=("--output $OUTPUT --auto")
+done < <(xrandr -q | sed -En 's/(\w+)\s+disconnected.+/\1/p')
+
 
 xrandr ${XRANDR_PARAMETERS[@]}
-
 exit 0
 
